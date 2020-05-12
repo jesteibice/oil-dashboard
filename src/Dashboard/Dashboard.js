e05171e719e2d1ec72a38d9d330bcf9659a3d939
Rope <jebachnakvadrat@gmail.com>
2020-05-13 00:11:15 +0200

    push me, ahhhhhhhhh

---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,314 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+// import Typography from '@material-ui/core/Typography';
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+// import { shadows } from '@material-ui/system';
+import Box from "@material-ui/core/Box";
+// import leftPane from "./leftPane";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 0,
+    overflow: "hidden",
+  },
+  rootright: {
+    flexGrow: 1,
+    marginTop: 0,
+    overflow: "hidden",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: "#828BC6",
+    backgroundColor: "#222655",
+    marginTop: "0.5em",
+    fontSize: "30px",
+    borderRadius: "0.3em",
+    fontWeight: 350,
+    letterSpacing: "0.17px",
+    lineHeight: "44px",
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+    minHeight: "10vh",
+  },
+  leftpane: {
+    minHeight: "75vh",
+  },
+  paperlefttop: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: "#828BC6",
+    position: "relative",
+    backgroundColor: "#222655",
+    marginTop: "0",
+    fontSize: "30px",
+    borderRadius: "0.3em",
+    fontWeight: 350,
+    letterSpacing: "0.17px",
+    lineHeight: "44px",
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: "0.7em",
+    // flexGrow: 5,
+    minHeight: "25vh",
+  },
+  paperleftbottom: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: "#828BC6",
+    position: "relative",
+    backgroundColor: "#222655",
+    marginTop: "0.7em",
+    fontSize: "30px",
+    borderRadius: "0.3em",
+    fontWeight: 350,
+    letterSpacing: "0.17px",
+    lineHeight: "44px",
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+    // flexGrow: 5,
+    minHeight: "49vh",
+  },
+
+  paperrightlefttop: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: "#828BC6",
+    position: "relative",
+    backgroundColor: "#222655",
+    marginTop: "0",
+    fontSize: "30px",
+    borderRadius: "0.3em",
+    fontWeight: 350,
+    letterSpacing: "0.17px",
+    lineHeight: "44px",
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+    // flexGrow: 5,
+    minHeight: "20vh",
+  },
+
+  paperrightleftbottom: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: "#828BC6",
+    position: "relative",
+    backgroundColor: "#222655",
+    marginTop: "0",
+    fontSize: "30px",
+    borderRadius: "0.3em",
+    fontWeight: 350,
+    letterSpacing: "0.17px",
+    lineHeight: "44px",
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+    // flexGrow: 5,
+    minHeight: "20vh",
+  },
+
+  paperrightright: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: "#828BC6",
+    position: "relative",
+    backgroundColor: "#222655",
+    marginTop: "0",
+    fontSize: "30px",
+    borderRadius: "0.3em",
+    fontWeight: 350,
+    letterSpacing: "0.17px",
+    lineHeight: "44px",
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+    // flexGrow: 5,
+    minHeight: "40vh",
+  },
+
+  papermidmid: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: "#828BC6",
+    position: "relative",
+    backgroundColor: "#222655",
+    marginTop: "0",
+    fontSize: "30px",
+    borderRadius: "0.3em",
+    fontWeight: 350,
+    letterSpacing: "0.17px",
+    lineHeight: "44px",
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+    minHeight: "37.5vh"
+  },
+  paperBottom: {
+    padding: theme.spacing(0.5),
+    textAlign: "center",
+    color: "#828BC6",
+    position: "relative",
+    backgroundColor: "#222655",
+    marginTop: "0",
+    marginBottom: "0",
+
+    fontSize: "13px",
+    lineHeight: "18px",
+    letterSpacing: "0.08px",
+    fontWeight: 300,
+    marginLeft: 0,
+    marginRight: 0,
+    paddingLeft: "2em",
+    paddingRight: "2em",
+    // paddingBottom: 0,
+    minHeight: "5vh",
+  },
+  paper2: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: "#828BC6",
+    position: "relative",
+    backgroundColor: "#222655",
+    marginTop: "0",
+    fontSize: "30px",
+    borderRadius: "0.3em",
+    fontWeight: 350,
+    letterSpacing: "0.17px",
+    lineHeight: "44px",
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: "0.6em",
+    // flexGrow: 5,
+    minHeight: "17vh",
+  },
+  paper3: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: "#828BC6",
+    position: "relative",
+    backgroundColor: "#222655",
+    marginTop: "0",
+    fontSize: "30px",
+    borderRadius: "0.3em",
+    fontWeight: 350,
+    letterSpacing: "0.17px",
+    lineHeight: "44px",
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+    // flexGrow: 5,
+    minHeight: "17vh",
+  },
+  paper4: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: "#828BC6",
+    position: "relative",
+    backgroundColor: "#222655",
+    marginTop: "0",
+    fontSize: "30px",
+    borderRadius: "0.3em",
+    fontWeight: 350,
+    letterSpacing: "0.17px",
+    lineHeight: "44px",
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: "0.7em",
+    // flexGrow: 5,
+    minHeight: "36.5vh",
+  },
+}));
+
+function Dashboard() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container
+        maxWidth="xl"
+        style={{ backgroundColor: "#1B1F44", height: "100vh" }}
+      >
+        <div className={classes.root}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Box
+                bgcolor="#1B1F44"
+                m={8}
+                p={8}
+                boxShadow={6}
+                color="text.primary"
+                clone
+              >
+                <Paper className={classes.paper}>
+                  Worldwide Oil Production
+                </Paper>
+              </Box>
+            </Grid>
+
+            <Grid item xs={3} className={classes.leftpane}>
+              <Paper className={classes.paperlefttop}>xs=3</Paper>
+              <Paper className={classes.paperleftbottom}>xs=3</Paper>
+            </Grid>
+            <Grid item xs={5}>
+              {/* <Paper className={classes.papermidmid}>xs=6</Paper> */}
+            </Grid>
+            <Grid item xs>
+              <Grid
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="stretch"
+                spacing={2}
+              >
+                {" "}
+                <Grid item xs={8}>
+                  <Paper className={classes.paper2}>xs=12</Paper>
+                  <Paper className={classes.paper3}>xs=12</Paper>
+                </Grid>
+                <Grid item xs={4}>
+                  <Paper className={classes.paper4}>xs=12</Paper>
+                </Grid>
+              </Grid>
+              <Grid item xs>
+                  <Paper className={classes.papermidmid}>xs=12</Paper>
+                </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <Box
+                bgcolor="#1B1F44"
+                m={8}
+                p={8}
+                boxShadow={6}
+                color="text.primary"
+                clone
+              >
+                <Paper className={classes.paperBottom}>
+                  *Source EIA (www.eia.gov) and national statistics sources.
+                  Petroleum supply includes the production of crude oil
+                  (including lease condensate), natural gas plant liquids, and
+                  it also includes refinery processing gain for volume (TBPD)
+                  only. Petroleum and other liquids consumption includes all
+                  domestic use and international bunkering of refined products,
+                  refinery fuel, and where available, direct combustion of crude
+                  oil and refinery by-products.
+                </Paper>
+              </Box>
+            </Grid>
+          </Grid>
+        </div>
+
+        {/* <Typography component="div" style={{ backgroundColor: '#1B1F44', height: '100vh' }} /> */}
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default Dashboard;
