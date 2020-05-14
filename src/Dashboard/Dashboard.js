9ed4173408ad0d38315eef1b97c5bbc86b580b60
Rope <jebachnakvadrat@gmail.com>
2020-05-14 23:51:28 +0200

    bump

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -12,11 +12,21 @@ import PerfectScrollbar from "react-perfect-scrollbar";
 // import WorldMap from 'react-world-map';
 // var WorldMap = require('react-world-map');
 // import { ReactWorldCountriesMap } from "react-world-countries-map"
-import {GeoMap} from "@corps-ui/maps";
+import { GeoMap } from "@corps-ui/maps";
 import countries from "../countries.json";
+import axios from "axios";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+} from "react-simple-maps";
 
+// url to a valid topojson file
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
-
+var numeral = require("numeral");
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -191,90 +201,161 @@ const useStyles = makeStyles((theme) => ({
     // flexGrow: 5,
     minHeight: "38.5vh",
   },
-  boxc:{
+  boxc: {
     minHeight: "41.6vh",
 
     height: "41.6vh",
-  }
+  },
 }));
 
-window.addEventListener('WorldMapClicked', function(e) {console.log('map was clicked, current selection is: ', e.detail.clickedState)});
-
+// window.addEventListener('WorldMapClicked', function(e) {console.log('map was clicked, current selection is: ', e.detail.clickedState)});
 
 function Dashboard() {
   const classes = useStyles();
 
   const [map, setMap] = useState(false);
-  const [stylePath, setStylePath] = useState('Dashboard.css')
+  const [wprod, setWprod] = useState("");
+  const [wconsum, setWconsum] = useState("");
+  const [brent, setBrent] = useState("");
+  const [wti, setWti] = useState("");
 
-  if (map) {
-    console.log('loaded')
-    // var mapa = document.querySelectorAll("iframe").contentWindow;
-    // console.log(mapa)
-    // const hx = sx;
-    // document.getElementsByTagName("svg").sytle.cssText = "background: blue;";
-//     var styleEl = document.createElement('style');
-// styleEl.innerHTML = 'svg { background-color: blue ;}';
-// document.head.appendChild(styleEl);
-setTimeout(function(){
-  //do what you need here
-  setStylePath("Map.css")
-}, 2000);
+  //   if (map) {
+  //     console.log('loaded')
+  //     // var mapa = document.querySelectorAll("iframe").contentWindow;
+  //     // console.log(mapa)
+  //     // const hx = sx;
+  //     // document.getElementsByTagName("svg").sytle.cssText = "background: blue;";
+  // //     var styleEl = document.createElement('style');
+  // // styleEl.innerHTML = 'svg { background-color: blue ;}';
+  // // document.head.appendChild(styleEl);
+  // setTimeout(function(){
+  //   //do what you need here
+  //   setStylePath("Map.css")
+  // }, 2000);
 
-  }
+  //   }
 
-// var obj = JSON.stringify(countries)
-var i;
-var array1 = new Array;
-var array2 = new Array;
-var array3 = new Array;
+  // var obj = JSON.stringify(countries)
+  // var i;
+  // var array1 = new Array;
+  // var array2 = new Array;
+  // var array3 = new Array;
 
-for (i = 0; i < 250; i++) {
-  var fil1 = (countries[i]["ISO3166-1-Alpha-3"]);
-  // var fil1 = (countries[i]["ISO3166-1-Alpha-3"] + " " + i);
-  var bycountryconsum = "http://api.eia.gov/series/?api_key=c10de4f134f66672b5c80ff6c0eda8c4&series_id=INTL.5-2-" + fil1 + "-TBPD.A";
-  var bycountryprod = "http://api.eia.gov/series/?api_key=c10de4f134f66672b5c80ff6c0eda8c4&series_id=INTL.53-1-" + fil1 + "-TBPD.M";
-  array1.push(bycountryconsum);
-  array2.push(bycountryprod)
-}
-setTimeout(function(){
-  //do what you need here
-  for (i = 0; i < 50; i++) {
-  // console.log(array2[i])
-  // console.log(array1[i]);
-  fetch(array2[i], { 
-    method: 'GET'
-  })
-  .then(function(response) { return response.json(); })
-  .then(function(json) {
-    // console.log(json.series[0].data[0])
-    // var result = {};
-    // result["key"] = {}
-    // result["key"].json = json 
-    array3.push(json)
-    // console.log(json.series)
-    
-    // use the json
-  });
-  }
-//   array3.sort(function(a, b){return b-a});
+  // for (i = 0; i < 250; i++) {
+  //   var fil1 = (countries[i]["ISO3166-1-Alpha-3"]);
+  //   // var fil1 = (countries[i]["ISO3166-1-Alpha-3"] + " " + i);
+  //   var bycountryconsum = "http://api.eia.gov/series/?api_key=c10de4f134f66672b5c80ff6c0eda8c4&series_id=INTL.5-2-" + fil1 + "-TBPD.A";
+  //   var bycountryprod = "http://api.eia.gov/series/?api_key=c10de4f134f66672b5c80ff6c0eda8c4&series_id=INTL.53-1-" + fil1 + "-TBPD.M";
+  //   array1.push(bycountryconsum);
+  //   array2.push(bycountryprod)
+  // }
+  // setTimeout(function(){
+  //   //do what you need here
+  //   for (i = 0; i < 50; i++) {
+  //   // console.log(array2[i])
+  //   // console.log(array1[i]);
+  //   fetch(array2[i], {
+  //     method: 'GET'
+  //   })
+  //   .then(function(response) { return response.json(); })
+  //   .then(function(json) {
+  //     // console.log(json.series[0].data[0])
+  //     // var result = {};
+  //     // result["key"] = {}
+  //     // result["key"].json = json
+  //     array3.push(json)
+  //     // console.log(json.series)
 
-//   array3.sort(function(a, b) {
-//     return parseFloat(a.price) - parseFloat(b.price);
-// });
-  console.log(array3)
-  // console.log(array3.series.data)
+  //     // use the json
+  //   });
+  //   }
+  // //   array3.sort(function(a, b){return b-a});
 
+  // //   array3.sort(function(a, b) {
+  // //     return parseFloat(a.price) - parseFloat(b.price);
+  // // });
+  //   console.log(array3)
+  //   // console.log(array3.series.data)
 
-  // console.log(array3.json[0].series[0].data[0])
-}, 2500);
+  //   // console.log(array3.json[0].series[0].data[0])
+  // }, 2500);
 
+  axios
+    .get(
+      "http://api.eia.gov/series/?api_key=c10de4f134f66672b5c80ff6c0eda8c4&series_id=INTL.53-1-WORL-TBPD.M"
+    )
+    .then(
+      (response) => {
+        var wordprod = response.data;
+        var wordprod2 = wordprod.series[0].data[0][1];
+        var wordprod22 = wordprod2.toFixed(2);
+        var wordprod3 = wordprod22.toString().split(".").join("");
+        wordprod3 += 0;
+        // var wordprod3 = wordprod2.toString();
+        var string = numeral(wordprod3).format("0.00a");
+        var end = string.replace(/m/g, "");
+        setWprod(end);
+        console.log(wordprod3);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 
+  axios
+    .get(
+      "http://api.eia.gov/series/?api_key=c10de4f134f66672b5c80ff6c0eda8c4&series_id=INTL.5-2-WORL-TBPD.A"
+    )
+    .then(
+      (response) => {
+        var wordconsum = response.data;
+        var wordconsum2 = wordconsum.series[0].data[0][1];
+        var wordconsum3 = wordconsum2.toFixed(3);
+        var wordconsum4 = wordconsum3.toString().split(".").join("");
+        var string2 = numeral(wordconsum4).format("0.00a");
+        var end2 = string2.replace(/m/g, "");
+        setWconsum(end2);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+  axios
+    .get(
+      "https://api.eia.gov/series/?api_key=c10de4f134f66672b5c80ff6c0eda8c4&series_id=PET.RBRTE.D"
+    )
+    .then(
+      (response) => {
+        var brent = response.data;
+        var brent2 = brent.series[0].data[0][1];
+        var brent3 = numeral(brent2).format("$00.0");
+        setBrent(brent3);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+  axios
+    .get(
+      "https://api.eia.gov/series/?api_key=c10de4f134f66672b5c80ff6c0eda8c4&series_id=PET.RWTC.D"
+    )
+    .then(
+      (response) => {
+        var wti = response.data;
+        var wti2 = wti.series[0].data[0][1];
+        var wti3 = numeral(wti2).format("$00.0");
+        setWti(wti3);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
 
   return (
     <React.Fragment>
       <CssBaseline />
-      <link rel="stylesheet" type="text/css" href={stylePath} />
       <Container
         maxWidth="xl"
         style={{ backgroundColor: "#1C1E43", height: "100vh" }}
@@ -307,7 +388,7 @@ setTimeout(function(){
               >
                 <Paper className={classes.paperlefttop}>
                   <div>World Oil Production</div>
-                  <label className="bigwhite">17.87</label>
+                  <label className="bigwhite">{wprod}</label>
                   <label className="underbig"> million barrel per day</label>
                 </Paper>
               </Box>
@@ -385,17 +466,28 @@ setTimeout(function(){
         position="relative"
         sandbox
         styles={{background: "black"}}/> */}
-        <div className="map">
-        {/* <GeoMap
+              <div className="map">
+                {/* <GeoMap
                 displayMode="2D"
                 // basemap="streets"
                 zoom={2}
             /> */}
-            {/* <iframe src="https://knoema.com/atlas/embed/topics/Energy/Oil/Production-of-crude-oil?type=maps" allowtransparency="true" frameborder="0" width="920" min-width="560" height="700" scrolling="no"></iframe> */}</div>
-                    </Grid>
-                    
+                {/* <iframe src="https://knoema.com/atlas/embed/topics/Energy/Oil/Production-of-crude-oil?type=maps" allowtransparency="true" frameborder="0" width="920" min-width="560" height="700" scrolling="no"></iframe> */}
+                <ComposableMap>
+                <ZoomableGroup>
+                  <Geographies geography={geoUrl}>
+                    {({ geographies }) =>
+                      geographies.map((geo) => (
+                        <Geography key={geo.rsmKey} geography={geo} />
+                      ))
+                    }
+                  </Geographies>
+                  </ZoomableGroup>
+                </ComposableMap>{" "}
+              </div>
+            </Grid>
+
             <Grid item xs>
-              
               <Grid
                 container
                 direction="row"
@@ -405,54 +497,53 @@ setTimeout(function(){
               >
                 {" "}
                 <Grid item xs={8}>
-                <div className={classes.boxc} spacing={2}>
-                  <Box
-                    bgcolor="#1B1F44"
-                    m={8}
-                    p={8}
-                    boxShadow={6}
-                    color="text.primary"
-                    clone
-                  >
-                    <Paper className={classes.paper2}>
-                      <div>World Oil Consumption</div>
-                      <label className="bigwhite2">93</label>
-                      <label className="underbig2">
-                        {" "}
-                        million barrel per day
-                      </label>
-                    </Paper>
-                  </Box>
-                  <Box
-                    bgcolor="#1B1F44"
-                    m={8}
-                    p={8}
-                    boxShadow={6}
-                    color="text.primary"
-                    clone
-                  >
-                    <Paper className={classes.paper3}>
-                      <div>Top 3 Oil Consumers</div>
-                      <div className="maintable2">
-                        <div>
-                          <div className="table2">
-                            <div className="left_col">Country 1</div>
-                            <div className="right_col">5,600,200</div>
+                  <div className={classes.boxc} spacing={2}>
+                    <Box
+                      bgcolor="#1B1F44"
+                      m={8}
+                      p={8}
+                      boxShadow={6}
+                      color="text.primary"
+                      clone
+                    >
+                      <Paper className={classes.paper2}>
+                        <div>World Oil Consumption</div>
+                        <label className="bigwhite2">{wconsum}</label>
+                        <label className="underbig2">
+                          {" "}
+                          million barrel per day
+                        </label>
+                      </Paper>
+                    </Box>
+                    <Box
+                      bgcolor="#1B1F44"
+                      m={8}
+                      p={8}
+                      boxShadow={6}
+                      color="text.primary"
+                      clone
+                    >
+                      <Paper className={classes.paper3}>
+                        <div>Top 3 Oil Consumers</div>
+                        <div className="maintable2">
+                          <div>
+                            <div className="table2">
+                              <div className="left_col">Country 1</div>
+                              <div className="right_col">5,600,200</div>
+                            </div>
+                            <div className="table2">
+                              <div className="left_col">Country 1</div>
+                              <div className="right_col">5,600,200</div>
+                            </div>{" "}
+                            <div className="table2">
+                              <div className="left_col">Country 1</div>
+                              <div className="right_col">5,600,200</div>
+                            </div>{" "}
                           </div>
-                          <div className="table2">
-                            <div className="left_col">Country 1</div>
-                            <div className="right_col">5,600,200</div>
-                          </div>{" "}
-                          <div className="table2">
-                            <div className="left_col">Country 1</div>
-                            <div className="right_col">5,600,200</div>
-                          </div>{" "}
                         </div>
-                      </div>
-                    </Paper>
-                  </Box>
+                      </Paper>
+                    </Box>
                   </div>
-
                 </Grid>
                 <Grid item xs={4}>
                   <Box
@@ -466,11 +557,11 @@ setTimeout(function(){
                     <Paper className={classes.paper4}>
                       <div className="half halfone">
                         <div className="brent">Brent</div>
-                        <div className="bigwhite3">$20.7</div>
+                        <div className="bigwhite3">{brent}</div>
                       </div>
                       <div className="half halftwo">
-                        <div className="brent">Brent</div>
-                        <div className="bigwhite4">$20.7</div>
+                        <div className="brent">WTI</div>
+                        <div className="bigwhite4">{wti}</div>
                       </div>
                     </Paper>
                   </Box>
